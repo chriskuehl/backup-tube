92de5e13a35034fbe7ad454ee97fd78ba93b499e
Chris Kuehl <chris@techxonline.net>
2010-12-31 18:54:51 +0000

    moved client functionality to the command-line app
    --HG--
    extra : convert_revision : svn%3Aad8ad6a5-8e25-0918-4fc4-e70d8e09d18f/trunk%4012

--- helper/src/net/kuehldesign/backuptube/BackupClient.java
+++ helper/src/net/kuehldesign/backuptube/BackupClient.java
@@ -14,7 +14,6 @@ import java.net.URLConnection;
 import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.List;
-import net.kuehldesign.backuptube.exception.BadVideoException;
 import net.kuehldesign.backuptube.exception.FatalBackupException;
 import net.kuehldesign.backuptube.exception.MalformedFeedURLException;
 import net.kuehldesign.backuptube.exception.UnableToGetSourceException;
@@ -163,83 +162,6 @@ public class BackupClient {
     public static String escapeFileName(String fileName) {
         fileName = fileName.replaceAll("/", "-");
         return fileName;
-    }
-
-    public static void main(String[] args) {
-        String user = args[0];
-        String saveDir = args[1];
-
-        BackupClient client = new BackupClient();
-        client.setUser(user);
-
-        ArrayList<YouTubeVideo> videos = null;
-
-        try {
-            videos = client.getVideos();
-        } catch (FatalBackupException ex) {
-            System.err.println("Fatal exception: " + ex.getMessage());
-            ex.printStackTrace();
-        } catch (UnableToOpenURLConnectionException ex) {
-            System.err.println("Unable to open URL connection; does YouTube account exist?");
-            ex.printStackTrace();
-        }
-
-        // now, start downloading the videos
-        try {
-            int videoCount = 0;
-            int totalVideoCount = videos.size();
-
-            for (YouTubeVideo video : videos) {
-                for (int downloadTry = 0; downloadTry < 3; downloadTry ++) {
-                    System.out.println("Starting try " + (downloadTry + 1) + "/3 to download \"" + video.getTitle() + "\"");
-
-                    try {
-                        video.init();
-                        String downloadURL = video.getDownloadURL();
-                        FileDownloader d = new FileDownloader(new URL(downloadURL), saveDir + escapeFileName(video.getTitle()) + "." + video.getExtension());
-                        d.startDownload();
-                        boolean hasError = false;
-
-                        while (! d.isFinished()) {
-                            if (d.hasError()) {
-                                hasError = true;
-                                break;
-                            }
-
-                            double progress = d.getProgress();
-                            progress *= 10000;
-                            progress = Math.round(progress);
-                            progress /= 100;
-
-                            System.out.println("Progress (" + videoCount + "/" + totalVideoCount + " \"" + video.getTitle() + "\"): " + progress + "%");
-
-                            try {
-                                Thread.sleep(1000);
-                            } catch (InterruptedException ex) {
-
-                            }
-                        }
-
-                        if (hasError) {
-                            System.out.println("Has error, starting new try.");
-                            continue;
-                        }
-
-                        System.out.println("Successfully downloaded video \"" + video.getTitle() + "\"");
-                        break;
-                    } catch (BadVideoException ex) {
-                        ex.printStackTrace();
-                    } catch (MalformedURLException ex) {
-                        ex.printStackTrace();
-                    }
-                }
-
-                videoCount ++;
-            }
-        } catch (FatalBackupException ex) {
-            System.err.println("Fatal exception: " + ex.getMessage());
-            ex.printStackTrace();
-        }
     }
 
 }
