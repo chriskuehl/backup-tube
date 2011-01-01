3ee07634830869e816442093ab48b051396d1c07
Chris Kuehl <chris@techxonline.net>
2011-01-01 01:33:58 +0000

    added jnetutils as a dep; using FileDownloader from jnetutils now that it was removed from backuptube classes
    --HG--
    extra : convert_revision : svn%3Aad8ad6a5-8e25-0918-4fc4-e70d8e09d18f/trunk%4028

--- app/src/net/kuehldesign/backuptube/app/BackupTubeApp.java
+++ app/src/net/kuehldesign/backuptube/app/BackupTubeApp.java
@@ -9,12 +9,12 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
 import net.kuehldesign.backuptube.BackupHelper;
-import net.kuehldesign.backuptube.FileDownloader;
 import net.kuehldesign.backuptube.app.exception.UnableToReadFromConsoleException;
 import net.kuehldesign.backuptube.exception.BadVideoException;
 import net.kuehldesign.backuptube.exception.FatalBackupException;
 import net.kuehldesign.backuptube.exception.UnableToOpenURLConnectionException;
 import net.kuehldesign.backuptube.video.YouTubeVideo;
+import net.kuehldesign.jnetutils.FileDownloader;
 
 public class BackupTubeApp {
     private static final String badSaveDirMessage = "Unable to create directory there, please choose a different location";
