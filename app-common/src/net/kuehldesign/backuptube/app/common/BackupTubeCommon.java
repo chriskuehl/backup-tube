a3b7dbe3ef31954243ed0b87cfb4d48b0bccd3b0
Chris Kuehl <chris@techxonline.net>
2011-01-02 02:15:14 +0000

    moved from command-line app to common: escapeFileName, isGoodSaveDir, fileExists, and fixDir
    --HG--
    extra : convert_revision : svn%3Aad8ad6a5-8e25-0918-4fc4-e70d8e09d18f/trunk%4038

--- app-common/src/net/kuehldesign/backuptube/app/common/BackupTubeCommon.java
+++ app-common/src/net/kuehldesign/backuptube/app/common/BackupTubeCommon.java
@@ -1,5 +1,47 @@
 package net.kuehldesign.backuptube.app.common;
 
-public class BackupTubeCommon {
+import java.io.File;
 
+public class BackupTubeCommon {
+    public static String escapeFileName(String fileName) {
+        String newFileName = "";
+        String alphanumeric = "abcdefghijklmnopqrstuvwxyzABCDEFGHIZJKLMNOPQRSTUV0234567890 ";
+
+        // make it alphanumeric for max compatibility
+        for (int i = 0; i < fileName.length(); i ++) {
+            String c = fileName.substring(i, i + 1);
+
+            if (alphanumeric.indexOf(c) > (- 1)) {
+                newFileName += c;
+            }
+        }
+
+        return newFileName;
+    }
+
+    public static boolean isGoodSaveDir(String saveDir) {
+        if (! fileExists(saveDir)) {
+            boolean wasCreated = new File(saveDir).mkdirs();
+
+            if (! wasCreated) {
+                return false; // permission error etc
+            } else {
+                return true;
+            }
+        } else {
+            return true;
+        }
+    }
+
+    public static boolean fileExists(String saveDir) {
+        return new File(saveDir).exists();
+    }
+
+    public static String fixDir(String dir) {
+        if (! dir.endsWith("/")) {
+            dir += "/";
+        }
+
+        return dir;
+    }
 }
