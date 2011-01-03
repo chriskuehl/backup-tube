da585a815ac1b261aded37397ef8a96e447dcf2b
Chris Kuehl <chris@techxonline.net>
2011-01-03 04:54:35 +0000

    added BackupTubeDataFile which will be a JSON feed (using GSON)
    --HG--
    extra : convert_revision : svn%3Aad8ad6a5-8e25-0918-4fc4-e70d8e09d18f/trunk%4046

---
+++ app-common/src/net/kuehldesign/backuptube/app/common/datafile/BackupTubeDataFile.java
@@ -0,0 +1,30 @@
+// used with GSON for storage of data
+
+package net.kuehldesign.backuptube.app.common.datafile;
+
+import java.util.LinkedList;
+import net.kuehldesign.backuptube.video.YouTubeVideo;
+
+
+public class BackupTubeDataFile {
+    // videos will be looped through to check for videos which have been deleted
+    // and also possibly to display a library -- not sure yet
+    private LinkedList<YouTubeVideo> videos;
+    private long lastUpdated;
+
+    public void setVideos(LinkedList<YouTubeVideo> videos) {
+        this.videos = videos;
+    }
+
+    public LinkedList<YouTubeVideo> getVideos() {
+        return videos;
+    }
+
+    public void setLastUpdated(long lastUpdated) {
+        this.lastUpdated = lastUpdated;
+    }
+
+    public long getLastUpdated() {
+        return lastUpdated;
+    }
+}
