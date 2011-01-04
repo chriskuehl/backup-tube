63a6b39bd14cb3f15e48981eb9769ff544d5852e
Chris Kuehl <chris@techxonline.net>
2011-01-04 17:58:13 +0000

    added title to StoredVideo
    --HG--
    extra : convert_revision : svn%3Aad8ad6a5-8e25-0918-4fc4-e70d8e09d18f/trunk%4078

--- app-common/src/net/kuehldesign/backuptube/app/common/stored/StoredVideo.java
+++ app-common/src/net/kuehldesign/backuptube/app/common/stored/StoredVideo.java
@@ -3,6 +3,15 @@ package net.kuehldesign.backuptube.app.common.stored;
 public class StoredVideo {
     public String folderName;
     public long downloadedTime;
+    private String title;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
 
     public String getFolderName() {
         return folderName;
