9ecef13eb0ad49d03548655b376cca473cc9f099
Chris Kuehl <chris@techxonline.net>
2011-01-08 02:12:42 +0000

    changing save file names
    --HG--
    extra : convert_revision : svn%3Aad8ad6a5-8e25-0918-4fc4-e70d8e09d18f/trunk%40143

--- app-common/src/net/kuehldesign/backuptube/app/common/BackupTubeCommon.java
+++ app-common/src/net/kuehldesign/backuptube/app/common/BackupTubeCommon.java
@@ -11,8 +11,8 @@ public class BackupTubeCommon {
     public static final String LOCATION_VIDEOS = "Videos";
     public static final String LOCATION_VIDEOS_DELETED = "Deleted Videos";
     public static final String LOCATION_VIDEO = ""; // video file, relative to video's folder, if using another dir REQUIRES a trailing slash
-    public static final String LOCATION_VIDEO_DATAFILE = "video.txt"; // data for each video, relative to the video's folder
-    public static final String LOCATION_DATAFILE = "data.txt"; // data in the root directory with info on all videos
+    public static final String LOCATION_VIDEO_DATAFILE = "video.html"; // data for each video, relative to the video's folder
+    public static final String LOCATION_DATAFILE = "config.json"; // data in the root directory with info on all videos
 
     public static Gson getPrettyGson() {
         return new GsonBuilder().setPrettyPrinting().create();
