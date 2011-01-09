f9d84290965403184b320cd0507a7136483d0419
Chris Kuehl <chris@techxonline.net>
2011-01-09 05:15:50 +0000

    padding and text fields
    --HG--
    extra : convert_revision : svn%3Aad8ad6a5-8e25-0918-4fc4-e70d8e09d18f/trunk%40182

--- app-gui/src/net/kuehldesign/backuptube/app/gui/BackupTubeGraphicManager.java
+++ app-gui/src/net/kuehldesign/backuptube/app/gui/BackupTubeGraphicManager.java
@@ -1,16 +1,19 @@
 package net.kuehldesign.backuptube.app.gui;
 
 import java.awt.Container;
+import java.awt.Dimension;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.Insets;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 import javax.swing.SwingConstants;
-import javax.swing.border.EtchedBorder;
 
 public class BackupTubeGraphicManager {
     JFrame frame;
@@ -23,6 +26,7 @@ public class BackupTubeGraphicManager {
         JFrame newFrame = new JFrame("BackupTube");
         newFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         newFrame.setLayout(new GridBagLayout());
+        newFrame.setMinimumSize(new Dimension(400, 200));
 
         addMenuBarToFrame(newFrame);
         addControlsToFrame(newFrame);
@@ -52,48 +56,74 @@ public class BackupTubeGraphicManager {
     private void addControlsToFrame(JFrame frame) {
         JLabel label;
         JComboBox dropDown;
-        Container pane = frame.getContentPane();
+        JTextField textField;
+        GridBagConstraints constraints;
         
-        GridBagConstraints constraints = new GridBagConstraints();
+        Container pane = frame.getContentPane();
+        JPanel panel = new JPanel(new GridBagLayout());
+
+        constraints = new GridBagConstraints();
+        constraints.weightx = 1;
+        constraints.weighty = 1;
+        constraints.fill = GridBagConstraints.BOTH;
+        constraints.gridx = 0;
+        constraints.gridy = 0;
+        int padding = 10;
+        constraints.insets = new Insets(padding, padding, padding, padding);
+
+        pane.add(panel, constraints);
 
         // "Site:" label
         label = new JLabel("Site:");
-        label.setBorder(new EtchedBorder());
         label.setHorizontalAlignment(SwingConstants.RIGHT);
 
+        constraints = new GridBagConstraints();
         constraints.weightx = 0;
         constraints.weighty = 0;
         constraints.fill = GridBagConstraints.HORIZONTAL;
         constraints.gridx = 0;
         constraints.gridy = 0;
+        constraints.insets = new Insets(0, 0, 0, 5);
 
-        pane.add(label, constraints);
+        panel.add(label, constraints);
 
         // site dropdown
         String[] sites = {"YouTube"};
         dropDown = new JComboBox(sites);
 
+        constraints = new GridBagConstraints();
         constraints.weightx = 1;
         constraints.weighty = 0;
         constraints.fill = GridBagConstraints.HORIZONTAL;
         constraints.gridx = 1;
         constraints.gridy = 0;
 
-        pane.add(dropDown, constraints);
+        panel.add(dropDown, constraints);
 
         // "Site:" label
         label = new JLabel("User:");
-        label.setBorder(new EtchedBorder());
         label.setHorizontalAlignment(SwingConstants.RIGHT);
 
+        constraints = new GridBagConstraints();
         constraints.weightx = 0;
         constraints.weighty = 0;
         constraints.fill = GridBagConstraints.HORIZONTAL;
         constraints.gridx = 0;
         constraints.gridy = 1;
+        constraints.insets = new Insets(0, 0, 0, 5);
 
-        pane.add(label, constraints);
+        panel.add(label, constraints);
 
         // user input field
+        textField = new JTextField(20);
+
+        constraints = new GridBagConstraints();
+        constraints.weightx = 1;
+        constraints.weighty = 0;
+        constraints.fill = GridBagConstraints.HORIZONTAL;
+        constraints.gridx = 1;
+        constraints.gridy = 1;
+
+        panel.add(textField, constraints);
     }
 }
