5144463fd2ae4dcc21c6c9d9341261e2a5ab9b36
Chris Kuehl <chris@techxonline.net>
2011-01-09 05:01:33 +0000

    adding some controls
    --HG--
    extra : convert_revision : svn%3Aad8ad6a5-8e25-0918-4fc4-e70d8e09d18f/trunk%40181

--- app-gui/src/net/kuehldesign/backuptube/app/gui/BackupTubeGraphicManager.java
+++ app-gui/src/net/kuehldesign/backuptube/app/gui/BackupTubeGraphicManager.java
@@ -1,9 +1,16 @@
 package net.kuehldesign.backuptube.app.gui;
 
+import java.awt.Container;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
+import javax.swing.SwingConstants;
+import javax.swing.border.EtchedBorder;
 
 public class BackupTubeGraphicManager {
     JFrame frame;
@@ -15,6 +22,7 @@ public class BackupTubeGraphicManager {
     private JFrame createInterface() {
         JFrame newFrame = new JFrame("BackupTube");
         newFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        newFrame.setLayout(new GridBagLayout());
 
         addMenuBarToFrame(newFrame);
         addControlsToFrame(newFrame);
@@ -42,6 +50,50 @@ public class BackupTubeGraphicManager {
     }
 
     private void addControlsToFrame(JFrame frame) {
+        JLabel label;
+        JComboBox dropDown;
+        Container pane = frame.getContentPane();
         
+        GridBagConstraints constraints = new GridBagConstraints();
+
+        // "Site:" label
+        label = new JLabel("Site:");
+        label.setBorder(new EtchedBorder());
+        label.setHorizontalAlignment(SwingConstants.RIGHT);
+
+        constraints.weightx = 0;
+        constraints.weighty = 0;
+        constraints.fill = GridBagConstraints.HORIZONTAL;
+        constraints.gridx = 0;
+        constraints.gridy = 0;
+
+        pane.add(label, constraints);
+
+        // site dropdown
+        String[] sites = {"YouTube"};
+        dropDown = new JComboBox(sites);
+
+        constraints.weightx = 1;
+        constraints.weighty = 0;
+        constraints.fill = GridBagConstraints.HORIZONTAL;
+        constraints.gridx = 1;
+        constraints.gridy = 0;
+
+        pane.add(dropDown, constraints);
+
+        // "Site:" label
+        label = new JLabel("User:");
+        label.setBorder(new EtchedBorder());
+        label.setHorizontalAlignment(SwingConstants.RIGHT);
+
+        constraints.weightx = 0;
+        constraints.weighty = 0;
+        constraints.fill = GridBagConstraints.HORIZONTAL;
+        constraints.gridx = 0;
+        constraints.gridy = 1;
+
+        pane.add(label, constraints);
+
+        // user input field
     }
 }
